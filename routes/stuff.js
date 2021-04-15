b1c14622a82f88573c32d5cdd9fe66d1ad768dec
Anaïs <kevin.castel@protonmail.com>
2021-04-15 15:41:40 +0200

    Begin Modif'

--- routes/stuff.js
+++ routes/stuff.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, stuffCtrl.createSauce);
-router.put('/:id', auth,stuffCtrl.modifySauce);
+router.post('/', auth, multer, stuffCtrl.createSauce);
+router.put('/:id', auth, multer, stuffCtrl.modifySauce);
 router.delete('/:id', auth, stuffCtrl.deleteSauce);
 router.get('/:id', auth, stuffCtrl.getOneSauce);
 router.get('/', auth, stuffCtrl.getAllSauce);
